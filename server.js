eccf4ea165906739381cdbcbacc23a34aea4144a
Oakar <linn.998.mlm@gmail.com>
2023-02-15 19:38:19 +0630

    Create server and call index.html

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-const fs = require ("fs");
-const http = require ("http");
+const fs = require("fs");
+const http = require("http");
 const port = 3000;
 
-const server = http.createServer((req,res)=>{
-    const method = request.method;
-})
-server.listen(3000,()=>{
-    console.log(`Server is listen in port${port}`);
-})
+const server = http.createServer((request, res) => {
+//   const method = request.method;
+  const route = request.url;
+  if (route === "/") {
+    fs.readFile("index.html", (error, data) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  }else if(route ==="script.js"){
+    fs.readFile("script.js", (error,data)=>{
+        res.writeHead(200, {"Content-Type":"text/javascript"});
+        res.write(data);
+        return res.end();
+    })
+  }else{
+    res.writeHead(200, {"Content-Type":"text/plain"});
+    res.write("NOt of all");
+  };
+
+});
+server.listen(3000, () => {
+  console.log(`Server is listen in port${port}`);
+});
